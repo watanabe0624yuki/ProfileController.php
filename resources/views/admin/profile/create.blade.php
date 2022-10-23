418cf7c96df753b3029683c957f9b528aedbe3cc
y-watanabe <justin.crown381@gmaile.com>
2022-10-23 13:06:04 +0000

    Bladファイルの作成　add actionに描画する

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatable"content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>MyNews</title>
+    </head>
+    <body>
+        <h1>大阪名物と言えばたこ焼きです。</h1>
+    </body>
+    
+</html>
