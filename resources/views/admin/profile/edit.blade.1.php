581b6e4c5b3ccb217ddba0841d373571470f9da5
y-watanabe <justin.crown381@gmaile.com>
2022-10-25 09:40:24 +0000

    【PHP/Laravel】07 ユーザー認証を実装する　応用課題

---
+++ resources/views/admin/profile/edit.blade.1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatable"content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <title>近藤先生へ</title>
+    </head>
+    <body>
+        <h1>次回のメンタリングではナルシストルーの仮装をしている近藤先生を見たいです。︎</h1>
+    </body>
+    
+</html>
--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -1,14 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatable"content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <title>近藤先生へ</title>
-    </head>
-    <body>
-        <h1>次回のメンタリングではナルシストルーの仮装をしている近藤先生を見たいです。︎</h1>
-    </body>
-    
-</html>
+{{-- layouts/admin.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
+@section('title', '近藤先生へ')
+
+{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>次回のメンタリングではナルシストルーの仮装をしている近藤先生を見たいです。</h2>
+            </div>
+        </div>
+    </div>
+@endsection
