845631eeadfee8de09b7d1f99617876bf127a648
y-watanabe <justin.crown381@gmaile.com>
2022-10-26 14:34:31 +0000

    【PHP/Laravel】08 ニュース投稿画面を作成しよう応用課題

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -7,9 +7,14 @@ use Illuminate\Http\Request;
 
 class NewsController extends Controller
 {
-    //
     public function add()
     {
         return view('admin.news.create');
+    }
+    
+    public function create(Request $request)
+    {
+        // admin/news/createにリダイレクトする
+        return redirect('admin/news/create');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 use App\Http\Controllers\Admin\NewsController;
-Route::controller(NewsController::class)->prefix('admin')->group(function() {
-Route::get('news/create','add')->middleware('auth');
+
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
+Route::get('news/create','add')->name('news.add');
+Route::post('news/create','create')->name('news.create');
 });
+
+use App\Http\Controllers\Admin\NewsController;
+
+Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
+Route::post('profile/create','create')->name('profile.create');
+Route::post('profile/edit','update')->name('profile.update');
+});
+
 
 use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)->prefix('admin')->group(function() {
