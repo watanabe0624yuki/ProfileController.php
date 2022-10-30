c9aafa50e4ab397a344c54e8d114a6ef5b225323
y-watanabe <justin.crown381@gmaile.com>
2022-10-30 10:06:58 +0000

    【PHP/Laravel】11 投稿したニュースを更新/削除しよう 課題提出

--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -1,16 +1,12 @@
-{{-- layouts/admin.blade.phpを読み込む --}}
 @extends('layouts.admin')
+@section('title', 'ニュースの編集')
 
-{{-- admin.blade.phpの@yield('title')に'ニュースの新規作成'を埋め込む --}}
-@section('title', '近藤先生へ')
-
-{{-- admin.blade.phpの@yield('content')に以下のタグを埋め込む --}}
 @section('content')
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>最近、セブンイレブンで販売されているカボチャのスープにはまっています。近藤先生もぜひ食べてみてください。</h2>
-                <form action="{{ route('admin.profile.update') }}" method="post" enctype="multipart/form-data">
+                <h2>ニュース編集</h2>
+                <form action="{{ route('admin.news.update') }}" method="post" enctype="multipart/form-data">
                     @if (count($errors) > 0)
                         <ul>
                             @foreach($errors->all() as $e)
@@ -19,27 +15,15 @@
                         </ul>
                     @endif
                     <div class="form-group row">
-                        <label class="col-md-2" for="name">氏名</label>
+                        <label class="col-md-2" for="title">タイトル</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="name" value="{{ $profile_form->name }}">
+                            <input type="text" class="form-control" name="title" value="{{ $news_form->title }}">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-md-2" for="name">性別</label>
+                        <label class="col-md-2" for="body">本文</label>
                         <div class="col-md-10">
-                            <input type="text" class="form-control" name="gender" value="{{ $profile_form->gender }}">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-2" for="name">趣味</label>
-                        <div class="col-md-10">
-                            <input type="text" class="form-control" name="hobby" value="{{ $profile_form->hobby }}">
-                        </div>
-                    </div>
-                    <div class="form-group row">
-                        <label class="col-md-2" for="introduction">自己紹介</label>
-                        <div class="col-md-10">
-                            <textarea class="form-control" name="introduction" rows="20">{{ $profile_form->introduction }}</textarea>
+                            <textarea class="form-control" name="body" rows="20">{{ $news_form->body }}</textarea>
                         </div>
                     </div>
                     <div class="form-group row">
@@ -47,7 +31,7 @@
                         <div class="col-md-10">
                             <input type="file" class="form-control-file" name="image">
                             <div class="form-text text-info">
-                                設定中: {{ $profile_form->image_path }}
+                                設定中: {{ $news_form->image_path }}
                             </div>
                             <div class="form-check">
                                 <label class="form-check-label">
@@ -58,7 +42,7 @@
                     </div>
                     <div class="form-group row">
                         <div class="col-md-10">
-                            <input type="hidden" name="id" value="{{ $profile_form->id }}">
+                            <input type="hidden" name="id" value="{{ $news_form->id }}">
                             @csrf
                             <input type="submit" class="btn btn-primary" value="更新">
                         </div>
--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,12 @@ Route::get('/', function () {
 use App\Http\Controllers\Admin\NewsController;
 
 Route::controller(NewsController::class)->prefix('admin')->name('admin.')->middleware('auth')->group(function() {
-    Route::get('news/create','add')->name('news.add');
-    Route::post('news/create','create')->name('news.create');
+   Route::get('news/create','add')->name('news.add');
+   Route::post('news/create','create')->name('news.create');
+   Route::get('news', 'index')->name('news.index');
+   Route::get('news/edit', 'edit')->name('news.edit');
+   Route::post('news/edit', 'update')->name('news.update');
+   Route::get('news/delete', 'delete')->name('news.delete');
 });
 
 use App\Http\Controllers\Admin\ProfileController;
@@ -28,7 +32,10 @@ use App\Http\Controllers\Admin\ProfileController;
 Route::controller(ProfileController::class)->prefix('admin')->name('admin.')->group(function() {
     Route::get('profile/create','add')->name('profile.add');
     Route::post('profile/create','create')->name('profile.create');
+    Route::get('profile', 'index')->name('profile.index');
+    Route::get('profile/edit', 'edit')->name('profile.edit');
     Route::post('profile/edit','update')->name('profile.update');
+    Route::get('profile/delete', 'delete')->name('profile.delete');
 });
 
 Route::controller(AAAController::class)->group(function() {
