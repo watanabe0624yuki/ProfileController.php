769f943c69249971cb3f242faea6aa8cb5796645
y-watanabe <justin.crown381@gmaile.com>
2022-11-04 13:22:21 +0000

    【PHP/Laravel】16 Herokuへの画像のアップロード

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 use App\Models\News;
 use App\Models\History;
 use Carbon\Carbon;
+use Storage;
 
 class NewsController extends Controller
 {
@@ -27,8 +28,8 @@ class NewsController extends Controller
         
         // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
         if(isset($form['image'])) {
-            $path = $request->file('image')->store('public/image');
-            $news->image_path = basename($path);
+            $path = $request->file('s3')->putFile('/',$form['image'],'public');
+            $news->image_path = Storage::disk('s3')->url($path);
         } else {
             $news->image_path =null;
         }
@@ -82,8 +83,8 @@ class NewsController extends Controller
         if ($request->remove == 'true') {
             $news_form['image_path'] = null;
         } elseif ($request->file('image')) {
-            $path = $request->file('image')->store('public/image');
-            $news_form['image_path'] = basename($path);
+        $path = Storage::disk('s3')->putFile('/',$form['image'],'public');
+        $news->image_path = Storage::disk('s3')->url($path);
         } else {
             $news_form['image_path'] = $news->image_path;
         }
